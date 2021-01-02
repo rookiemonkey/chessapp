4840788798a9f7fd2d5163a21f4991a4495e4dec
rookiemonkey <kevinroirigorbasina@pm.me>
2021-01-02 15:18:22 +0800

    add attack moves for rook

--- src/assets/js/moves/rook.js
+++ src/assets/js/moves/rook.js
@@ -1,23 +1,63 @@
-
 export default function move_rook(props) {
     const { rowFrom, colFrom } = props;
+    const row = parseInt(rowFrom);
+    const col = parseInt(colFrom);
     const validMoves = new Array();
-    const currentCoor = [rowFrom, colFrom];
+    const oneRowUp = row - 1;
+    const oneRowDown = row + 1;
+    const oneColLeft = col - 1;
+    const oneColRight = col + 1;
 
-    currentCoor.forEach((coor, ind) => {
+    // ROW loop from one-row up to 1
+    if (oneRowUp >= 1) {
+        for (let r = oneRowUp; r >= 1; r--) {
+            const cell = document.querySelector(`[data-coor='${r}_${colFrom}']`)
 
-        for (let i = 1; i <= 8; i++) {
+            // stop once a chess piece is present
+            if (cell.id)
+                break;
 
-            if (ind == 0 && `${coor}_${i}` != `${rowFrom}_${colFrom}`) {
-                validMoves.push(`${coor}_${i}`)
-            }
-
-            if (ind == 1 && `${i}_${coor}` != `${rowFrom}_${colFrom}`) {
-                validMoves.push(`${i}_${coor}`)
-            }
+            validMoves.push(cell.getAttribute('data-coor'))
         }
+    }
 
-    })
+    // ROW loop from one-row down to 8
+    if (oneRowDown <= 8) {
+        for (let j = oneRowDown; j <= 8; j++) {
+            const cell = document.querySelector(`[data-coor='${j}_${colFrom}']`)
+
+            if (cell.id)
+                break; // stop once a chess piece is present
+
+            validMoves.push(cell.getAttribute('data-coor'))
+        }
+    }
+
+
+    // COL loop from one-col left to 1
+    if (oneColLeft >= 1) {
+        for (let k = oneColLeft; k >= 1; k--) {
+            const cell = document.querySelector(`[data-coor='${rowFrom}_${k}']`)
+
+            if (cell.id)
+                break; // stop once a chess piece is present
+
+            validMoves.push(cell.getAttribute('data-coor'))
+        }
+    }
+
+
+    // COL loop from one-col right to 8
+    if (oneColLeft <= 8) {
+        for (let l = oneColRight; l <= 8; l++) {
+            const cell = document.querySelector(`[data-coor='${rowFrom}_${l}']`)
+
+            if (cell.id)
+                break; // stop once a chess piece is present
+
+            validMoves.push(cell.getAttribute('data-coor'))
+        }
+    }
 
     return validMoves;
 }
