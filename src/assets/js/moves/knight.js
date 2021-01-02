07d46fae5220903eb38ee7137433014b50b1e813
rookiemonkey <kevinroirigorbasina@pm.me>
2021-01-02 17:10:31 +0800

    add attack moves for knight

--- src/assets/js/moves/knight.js
+++ src/assets/js/moves/knight.js
@@ -4,40 +4,92 @@ export default function move_knight(props) {
     const validMoves = new Array();
     const row = parseInt(rowFrom);
     const col = parseInt(colFrom);
-    const up = row - 2
-    const down = row + 2
-    const left = col - 2
-    const right = col + 2
+    const oneUp = row - 1
+    const twoUp = row - 2
+    const oneDown = row + 1
+    const twoDown = row + 2
+    const oneLeft = col - 1
+    const twoLeft = col - 2
+    const oneRight = col + 1
+    const twoRight = col + 2
 
-    if (up && up > 0) {
+
+
+
+    // check for existing pieces on knight's path UP
+    const cellOneUp = document.querySelector(`[data-coor='${oneUp}_${col}']`)
+    const cellTwoUp = document.querySelector(`[data-coor='${twoUp}_${col}']`)
+
+    if (twoUp && twoUp > 0 && !cellOneUp.id && !cellTwoUp.id) {
         const onItsRight = col + 1;
         const onItsLeft = col - 1;
-        onItsRight > 0 ? validMoves.push(`${up}_${onItsRight}`) : null;
-        onItsLeft > 0 ? validMoves.push(`${up}_${onItsLeft}`) : null;
-    }
 
-    if (down && down > 0 && down <= 8) {
-        const onItsRight = col + 1;
-        const onItsLeft = col - 1;
-        onItsRight > 0 ? validMoves.push(`${down}_${onItsRight}`) : null;
-        onItsLeft > 0 ? validMoves.push(`${down}_${onItsLeft}`) : null;
-    }
+        onItsRight > 0
+            ? validMoves.push(`${twoUp}_${onItsRight}`)
+            : null;
 
-    if (left && left > 0) {
-        const onItsTop = row - 1;
-        const onItsBottom = row + 1;
-        onItsTop > 0 ? validMoves.push(`${onItsTop}_${left}`) : null;
-        onItsBottom > 0 && onItsBottom <= 8
-            ? validMoves.push(`${onItsBottom}_${left}`)
+        onItsLeft > 0
+            ? validMoves.push(`${twoUp}_${onItsLeft}`)
             : null;
     }
 
-    if (right && right > 0 && right <= 8) {
+
+
+
+    // check for existing pieces on knight's path DOWN
+    const cellOneDown = document.querySelector(`[data-coor='${oneDown}_${col}']`)
+    const cellTwoDown = document.querySelector(`[data-coor='${twoDown}_${col}']`)
+
+    if (twoDown && twoDown > 0 && twoDown <= 8 && !cellOneDown.id && !cellTwoDown.id) {
+        const onItsRight = col + 1;
+        const onItsLeft = col - 1;
+
+        onItsRight > 0
+            ? validMoves.push(`${twoDown}_${onItsRight}`)
+            : null;
+
+        onItsLeft > 0
+            ? validMoves.push(`${twoDown}_${onItsLeft}`)
+            : null;
+    }
+
+
+
+
+    // check for existing pieces on knight's path LEFT
+    const cellOneLeft = document.querySelector(`[data-coor='${row}_${oneLeft}']`)
+    const cellTwoLeft = document.querySelector(`[data-coor='${row}_${twoLeft}']`)
+
+    if (twoLeft && twoLeft > 0 && !cellOneLeft.id && !cellTwoLeft.id) {
         const onItsTop = row - 1;
         const onItsBottom = row + 1;
-        onItsTop > 0 ? validMoves.push(`${onItsTop}_${right}`) : null;
+
+        onItsTop > 0
+            ? validMoves.push(`${onItsTop}_${twoLeft}`)
+            : null;
+
         onItsBottom > 0 && onItsBottom <= 8
-            ? validMoves.push(`${onItsBottom}_${right}`)
+            ? validMoves.push(`${onItsBottom}_${twoLeft}`)
+            : null;
+    }
+
+
+
+
+    // check for existing pieces on knight's path RIGHT
+    const cellOneRight = document.querySelector(`[data-coor='${row}_${oneRight}']`)
+    const cellTwoRight = document.querySelector(`[data-coor='${row}_${twoRight}']`)
+
+    if (twoRight && twoRight > 0 && twoRight <= 8 && !cellOneRight.id && !cellTwoRight.id) {
+        const onItsTop = row - 1;
+        const onItsBottom = row + 1;
+
+        onItsTop > 0
+            ? validMoves.push(`${onItsTop}_${twoRight}`)
+            : null;
+
+        onItsBottom > 0 && onItsBottom <= 8
+            ? validMoves.push(`${onItsBottom}_${twoRight}`)
             : null;
     }
 
